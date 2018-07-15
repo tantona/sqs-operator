7ebaebb661aa00adc3add04c13f63c92e2d0565a
Alex Tanton <tantona@gmail.com>
2018-07-15 11:55:27 -0500

    INITIAL COMMIT

---
+++ pkg/apis/stable/v1/types.go
@@ -0,0 +1,29 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type SQSQueueList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []SQSQueue `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type SQSQueue struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              SQSQueueSpec   `json:"spec"`
+	Status            SQSQueueStatus `json:"status,omitempty"`
+}
+
+type SQSQueueSpec struct {
+	// Fill me
+}
+type SQSQueueStatus struct {
+	// Fill me
+}
